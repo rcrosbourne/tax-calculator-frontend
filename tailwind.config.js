4987b69b460f33310a2d227f6229389b834ee385
Rainaldo <rcrosbourne@gmail.com>
2021-12-27 13:24:29 -0500

    Updated frontend page

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        island: ["Island Moments", "cursive"],
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 };
